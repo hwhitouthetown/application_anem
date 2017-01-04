73a21f7e2af3ff6f3e76eb3da9e23cdcd43a5ca9
Robin Delaporte <robin-delaporte@hotmail.fr>
2017-01-04 16:39:34 +0100

    lol

--- src/StageBundle/Controller/ApiController.php
+++ src/StageBundle/Controller/ApiController.php
@@ -2,16 +2,24 @@
 
 namespace StageBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use JMS\Serializer\SerializationContext;
+
+
+
+
 /**
  * @Route("/api/stage")
  */
-class DefaultController extends Controller
+class ApiController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('StageBundle:Default:index.html.twig');
-    }
     public function getEntrepriseAction(){
       $repository = $this
       ->getDoctrine()

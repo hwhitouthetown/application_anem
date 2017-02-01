409450ed196b3b353e5e4941c710ede9595cc514
Robin Delaporte <robin-delaporte@hotmail.fr>
2017-02-01 12:56:13 +0100

    getter setter pour User

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -38,5 +38,91 @@ class User extends BaseUser
     {
         return $this->id;
     }
+
+    /**
+     * Gets the value of nom.
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Sets the value of nom.
+     *
+     * @param string $nom the nom
+     *
+     * @return self
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Sets the value of id.
+     *
+     * @param int $id the id
+     *
+     * @return self
+     */
+    protected function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of prenom.
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Sets the value of prenom.
+     *
+     * @param string $prenom the prenom
+     *
+     * @return self
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of url.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Sets the value of url.
+     *
+     * @param string $url the url
+     *
+     * @return self
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
 }
 
